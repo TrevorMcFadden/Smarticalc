f77708bbce9a44aef87d898f7c3def74f9a8ca35
Trevor McFadden ✝️ <88262580+TrevorMcFadden@users.noreply.github.com>
2022-08-12 22:00:38 -0700

    Updated MainPage.xaml.vb

--- MainPage.xaml.vb
+++ MainPage.xaml.vb
@@ -1,4 +1,11 @@
 ﻿Public NotInheritable Class MainPage
     Inherits Page
-    'Note: Most of the coding is in the HTML file.
+    ''Note: Most of the coding is in the HTML file.
+    ''SmarticalcCommandBar subroutines
+    Private Sub RefreshButton_Click(sender As Object, e As RoutedEventArgs) Handles RefreshButton.Click
+        Navi.Refresh()
+    End Sub
+    Private Sub StopButton_Click(sender As Object, e As RoutedEventArgs) Handles RefreshButton.Click
+        Navi.Stop()
+    End Sub
 End Class
